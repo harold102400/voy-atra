27bb29183ebe49032b90fafafaf06ad1fa6b47ed
Luis_Eduardo <leduardo@evopointsolution.com>
2022-07-18 22:43:08 -0400

    .

--- src/components/pages/auth/auth.jsx
+++ src/components/pages/auth/auth.jsx
@@ -4,15 +4,15 @@ import { useNavigate } from 'react-router-dom';
 import { Formik, Form } from 'formik';
 import { UserAuth } from '../context/authContext';
 import Swal from 'sweetalert2';
-import YupValidationSchema from '../utils/yupValidationSchema';
-import Spinner from '../components/spinner/spinner';
+import YupValidationSchema from './utils/yupValidationSchema';
+import Spinner from '../../spinner/spinner';
 
 const Auth = () => {
   const SwalObj = Swal.mixin();
   const { signIn } = UserAuth();
   const [loadingData, setLoadingData] = useState(false);
   const navigate = useNavigate();
-  const validation = YupValidationSchema.signIn;
+  //const validation = YupValidationSchema.signIn;
   const initialValues = {
     email: '',
     password: ''
