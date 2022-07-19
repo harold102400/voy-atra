6c3e262a0f0b65106fc782d67cb3dfe8452d631b
Luis_Eduardo <leduardo@evopointsolution.com>
2022-07-18 22:57:33 -0400

    se agrega jsonConfig

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Router from './router/router';
 import { AuthContextProvider } from './context/authContext';
--- src/components/pages/auth/auth.jsx
+++ src/components/pages/auth/auth.jsx
@@ -2,21 +2,62 @@ import './auth.css';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Formik, Form } from 'formik';
-import { UserAuth } from '../context/authContext';
+import { UserAuth } from 'context/authContext';
 import Swal from 'sweetalert2';
-import YupValidationSchema from './utils/yupValidationSchema';
+import YupValidationSchema from 'utils/yupValidationSchema';
 import Spinner from '../../spinner/spinner';
+import InputFormik from 'components/formik/inputFormik';
 
 const Auth = () => {
   const SwalObj = Swal.mixin();
   const { signIn } = UserAuth();
   const [loadingData, setLoadingData] = useState(false);
   const navigate = useNavigate();
-  //const validation = YupValidationSchema.signIn;
+  const validation = YupValidationSchema.signIn;
   const initialValues = {
     email: '',
     password: ''
   };
+
+  const handleOnAuth = async (values) => {
+    setLoadingData(true);
+    try {
+      await signIn(values.email, values.password);
+      navigate('/admin-panel');
+      setLoadingData(false);
+    } catch (err) {
+      setLoadingData(false);
+      console.log(err);
+      SwalObj.fire({
+        html: `<strong>${err}</strong>`,
+        icon: 'error',
+        showConfirmButton: false
+      });
+    }
+  };
+
+  return loadingData ? (
+    <Spinner />
+  ) : (
+    <Formik initialValues={initialValues} validationSchema={validation} onSubmit={handleOnAuth}>
+      <div>
+        <h1 className='login-title'>Login</h1>
+        <div className='login-form'>
+          <Form>
+            <div className='form-control'>
+              <InputFormik control='input' type='email' label='Email:' name='email' />
+              <InputFormik control='input' type='password' label='Password:' name='password' />
+              <div className='d-grid gap-2 mt-3'>
+                <button className='btn btn-secondary btn-lg btn-block' type='submit'>
+                  Login
+                </button>
+              </div>
+            </div>
+          </Form>
+        </div>
+      </div>
+    </Formik>
+  );
 };
 
 export default Auth;
