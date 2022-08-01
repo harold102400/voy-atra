a8586c7f3d7dbe3819e7ab1a46ca0e9fb9ca2c91
Luis_Eduardo <leduardo@evopointsolution.com>
2022-08-01 08:16:46 -0400

    se agrega boton para solicitar citas

---
+++ src/components/buttons/requestAppointmentBtn.jsx
@@ -0,0 +1,11 @@
+import Button from 'react-bootstrap/Button';
+
+const RequestAppointmentBtn = () => {
+  return (
+    <div className='mt-5'>
+      <Button variant='btn btn-secondary btn-lg btn-block mt-3'>Solicitar Cita</Button>
+    </div>
+  );
+};
+
+export default RequestAppointmentBtn;
--- src/components/pages/home/home.jsx
+++ src/components/pages/home/home.jsx
@@ -1,10 +1,13 @@
 import AppointmentsView from 'components/appointmentsView/appointmentsView';
+import RequestAppointmentBtn from 'components/buttons/requestAppointmentBtn';
 
 const Home = () => {
   return (
-    <>
+    <div>
+      <h2>Listado de turnos</h2>
+      <RequestAppointmentBtn />
       <AppointmentsView />
-    </>
+    </div>
   );
 };
 
