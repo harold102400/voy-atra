45f6a01e86aed04847547c03adbc79772bba8ee2
Luis_Eduardo <leduardo@evopointsolution.com>
2022-08-01 18:39:43 -0400

    se agrega boton para volver atras

--- src/components/appointmentRequest/appointmentRequest.jsx
+++ src/components/appointmentRequest/appointmentRequest.jsx
@@ -4,6 +4,7 @@ import { Formik, Form } from 'formik';
 import InputFormik from 'components/formik/inputFormik';
 import YupValidationSchema from 'utils/yupValidationSchema';
 import Button from 'react-bootstrap/Button';
+import NavigateButton from 'components/buttons/navigateButton/navigateButton';
 
 const AppointmentRequest = () => {
   const validation = YupValidationSchema.appointment;
@@ -23,9 +24,9 @@ const AppointmentRequest = () => {
         <div className='appointment-form mt-5'>
           <Form>
             <InputFormik control='input' type='text' label='Nombre:' name='name' />
-            <br />
             <InputFormik control='input' type='text' label='Comentario:' name='comment' />
             <div className='mt-4'>
+              <NavigateButton route={'/'} variant='btn btn-outline-dark btn-lg btn-block' text={'Volver'} />
               <Button variant='btn btn-secondary btn-lg' type='submit'>
                 Solicitar
               </Button>
