a6a3c985a137f807420007e5bc7a538065d65495
Luis_Eduardo <leduardo@evopointsolution.com>
2022-08-01 07:03:07 -0400

    se agrega timeZone a la funcion de limpieza de colleccion

--- functions/index.js
+++ functions/index.js
@@ -3,12 +3,15 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 //delete all appointments at 12:00am
-exports.clearAllAppointments = functions.pubsub.schedule('0 0 * * *').onRun(async (context) => {
-  const db = admin.firestore();
-  const snap = await db.collection('appointments').get();
-  let promises = [];
-  snap.forEach((snap) => {
-    promises.push(snap.ref.delete());
+exports.clearAllAppointments = functions.pubsub
+  .schedule('0 0 * * *')
+  .timeZone('America/Santo_Domingo')
+  .onRun(async (context) => {
+    const db = admin.firestore();
+    const snap = await db.collection('appointments').get();
+    let promises = [];
+    snap.forEach((snap) => {
+      promises.push(snap.ref.delete());
+    });
+    return Promise.all(promises);
   });
-  return Promise.all(promises);
-});
