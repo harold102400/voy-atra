b86a824d9a1c90cc4c61a149c07cc0e29abb3490
Luis_Eduardo <leduardo@evopointsolution.com>
2022-08-01 08:57:58 -0400

    preparando form para solicitar turno

--- src/components/buttons/requestAppointmentBtn.jsx
+++ src/components/buttons/requestAppointmentBtn.jsx
@@ -1,10 +1,54 @@
+import React, { useState } from 'react';
+import { Formik, Form } from 'formik';
+import InputFormik from 'components/formik/inputFormik';
+import YupValidationSchema from 'utils/yupValidationSchema';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 const RequestAppointmentBtn = () => {
+  const [show, setShow] = useState(false);
+  const validation = YupValidationSchema.signIn;
+  const initialValues = {
+    name: '',
+    comment: ''
+  };
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const handleSubmit = async (values) => {
+    console.log(values);
+  };
+
   return (
-    <div className='mt-5'>
-      <Button variant='btn btn-secondary btn-lg btn-block mt-3'>Solicitar Cita</Button>
-    </div>
+    <>
+      <div className='mt-5'>
+        <Button variant='btn btn-secondary btn-lg btn-block mt-3' onClick={handleShow}>
+          Solicitar Turno
+        </Button>
+      </div>
+
+      <Modal show={show} onHide={handleClose} centered>
+        <Formik initialValues={initialValues} validationSchema={validation} onSubmit={handleSubmit}>
+          <Form>
+            <Modal.Header closeButton>
+              <Modal.Title>Solicitar Turno</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <InputFormik control='input' type='text' label='Nombre:' name='name' />
+              <InputFormik control='input' type='text' label='Comentario:' name='comment' />
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant='btn btn-outline-dark' onClick={handleClose}>
+                Cancelar
+              </Button>
+              <Button type='submit' variant='btn btn-secondary' onClick={handleClose}>
+                Solicitar Turno
+              </Button>
+            </Modal.Footer>
+          </Form>
+        </Formik>
+      </Modal>
+    </>
   );
 };
 
