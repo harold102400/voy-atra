628db6a1744a7b1aa4e379b1f7c4b6ed53b76c09
Luis_Eduardo <leduardo@evopointsolution.com>
2022-08-02 13:28:11 -0400

    .

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,9 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
 import ProtectedRoutes from './protectedRoutes';
-import NotFound from '../components/pages/notFound/notFound';
-import Home from '../components/pages/home/home';
-import Auth from '../components/pages/auth/auth';
-import AdminPanel from '../components/pages/adminPanel/adminPanel';
+import NotFound from 'components/pages/notFound/notFound';
+import Home from 'components/pages/home/home';
+import Auth from 'components/pages/auth/auth';
+import AdminPanel from 'components/pages/adminPanel/adminPanel';
 import AppointmentRequest from 'components/appointmentRequest/appointmentRequest';
 
 const Router = () => {
