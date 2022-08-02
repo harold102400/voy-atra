ac6c8b79629629ba9df7604509790fbf6900d296
Luis_Eduardo <leduardo@evopointsolution.com>
2022-08-02 13:17:20 -0400

    boton de agregar turno en pantalla de admin

--- src/components/pages/adminPanel/adminPanel.jsx
+++ src/components/pages/adminPanel/adminPanel.jsx
@@ -1,10 +1,13 @@
 import AppointmentsAdmin from "components/appointmentsAdmin/appointmentsAdmin";
+import NavigateBtn from 'components/buttons/navigateBtn/navigateBtn';
 
 const AdminPanel = () => {
   return (
     <div>
       <h2>Panel de Administración</h2>
-      
+      <div className='mt-5'>
+        <NavigateBtn route={'/appointment-request'} variant='btn btn-secondary btn-lg btn-block' text={'Agregar Turno'} />
+      </div>
       <AppointmentsAdmin />
     </div>
   );
