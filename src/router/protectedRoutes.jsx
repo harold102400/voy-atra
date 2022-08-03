46edeb2435ace6a5f94d8b4be566be380450f357
Luis_Eduardo <undernet1@gmail.com>
2022-08-03 13:28:25 -0400

    .

--- src/router/protectedRoutes.jsx
+++ src/router/protectedRoutes.jsx
@@ -1,13 +1,10 @@
 import { Outlet } from 'react-router-dom';
 import { UserAuth } from 'context/userContext';
-import { useParams, useNavigate } from 'react-router-dom';
 
 const ProtectedRoutes = () => {
   const { user } = UserAuth();
-  const params = useParams();
-  const navigate = useNavigate();
 
-  return user ? <Outlet /> : navigate(`/auth/${params.site}`);
+  return user ? <Outlet /> : <p>Forbidden.</p>;
 };
 
 export default ProtectedRoutes;
