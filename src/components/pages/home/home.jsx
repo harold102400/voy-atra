f5d3374c7dd0b3c7a5fd0fcd0f4a4679d1620f13
Luis_Eduardo <leduardo@evopointsolution.com>
2022-08-02 22:08:08 -0400

    se agrega boton desde home para ir a administracion

--- src/components/pages/home/home.jsx
+++ src/components/pages/home/home.jsx
@@ -1,15 +1,21 @@
+import './home.css';
 import { useParams } from 'react-router-dom';
+import { UserAuth } from 'context/authContext';
 import AppointmentsView from 'components/appointments/appointmentsView/appointmentsView';
 import NavigateBtn from 'components/buttons/navigateBtn/navigateBtn';
 
 const Home = () => {
   const params = useParams();
+  const { userSite } = UserAuth();
 
   return (
     <div>
       <h2>Listado de turnos</h2>
       <div className='mt-5'>
         <NavigateBtn route={`/appointment-request/${params.site}`} variant='btn btn-secondary btn-lg btn-block' text={'Solicitar Turno'} />
+        {userSite && (
+          <NavigateBtn route={`/admin-panel/${userSite}`} variant='outline-dark btn-lg btn-block' text={'Ir a Panel de Administración'} />
+        )}
       </div>
       <AppointmentsView site={params.site} />
     </div>
