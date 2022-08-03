5d052caf7d19663aad23ce8627bce5ed972389b2
Luis_Eduardo <leduardo@evopointsolution.com>
2022-08-02 20:31:05 -0400

    .

--- src/router/protectedRoutes.jsx
+++ src/router/protectedRoutes.jsx
@@ -1,10 +1,10 @@
-import { Navigate, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { UserAuth } from 'context/authContext';
 
 const ProtectedRoutes = () => {
   const { user } = UserAuth();
 
-  return user ? <Outlet /> : <Navigate to='/' />;
+  return user ? <Outlet /> : <div>Forbidden.</div>;
 };
 
 export default ProtectedRoutes;
