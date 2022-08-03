ae3f6ba8a4b5b0d5fac36cc2d0833a5c06911ecb
Luis_Eduardo <leduardo@evopointsolution.com>
2022-08-02 20:03:34 -0400

    se agregan las demas colecciones a la funcion de limpieza

--- functions/index.js
+++ functions/index.js
@@ -2,16 +2,29 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-//delete all appointments at 12:00am
+//delete all appointments from all collections at 12:00am
 exports.clearAllAppointments = functions.pubsub
   .schedule('0 0 * * *')
   .timeZone('America/Santo_Domingo')
   .onRun(async (context) => {
     const db = admin.firestore();
-    const snap = await db.collection('appointments').get();
     let promises = [];
+    let snap;
+
+    snap = await db.collection('appointments').get();
     snap.forEach((snap) => {
       promises.push(snap.ref.delete());
     });
+
+    snap = await db.collection('hamilton-barbershop').get();
+    snap.forEach((snap) => {
+      promises.push(snap.ref.delete());
+    });
+
+    snap = await db.collection('victor-barbershop').get();
+    snap.forEach((snap) => {
+      promises.push(snap.ref.delete());
+    });
+
     return Promise.all(promises);
   });
