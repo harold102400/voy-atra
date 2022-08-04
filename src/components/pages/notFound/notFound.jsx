845d10b6bda4fc8d2457ff303dba3021c1502266
Luis_Eduardo <leduardo@evopointsolution.com>
2022-08-03 21:08:14 -0400

    se elimina codigo del notFound para simplificar

--- src/components/pages/notFound/notFound.jsx
+++ src/components/pages/notFound/notFound.jsx
@@ -1,11 +1,5 @@
-import { UserAuth } from 'context/userContext';
-import { useNavigate } from 'react-router-dom';
-
 const NotFound = () => {
-  const { userSite } = UserAuth();
-  const navigate = useNavigate();
-
-  return userSite ? navigate(`/admin-panel/${userSite}`) : <p>Pagina no encontrada.</p>;
+  return <p>Pagina no encontrada.</p>;
 };
 
 export default NotFound;
